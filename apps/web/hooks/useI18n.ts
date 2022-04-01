f46f88e733d183977559b37a3d8a17fb2f6fcd80
Eunsoo Lee <cbcruk@users.noreply.github.com>
2022-04-02 07:36:12 +0900

    fix: type에러 수정을 위한 t 함수 type 추가

--- apps/web/hooks/useI18n.ts
+++ apps/web/hooks/useI18n.ts
@@ -1,9 +1,11 @@
 import { useContext } from 'react'
+import { Rosetta } from 'rosetta'
 import { I18nContext } from '../components/I18n/context'
 
 function useI18n() {
   const i18n = useContext<{
     activeLocale: string
+    t: Rosetta<any>['t']
   }>(I18nContext)
   return i18n
 }
