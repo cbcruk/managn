ee158a59312df8847a0a09ed47d313a08548d639
Eunsoo Lee <cbcruk@users.noreply.github.com>
2023-01-04 21:17:23 +0900

    config: 이미지 도메인 추가

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,7 +3,7 @@ const withTM = require('next-transpile-modules')(['ui'])
 module.exports = withTM({
   reactStrictMode: true,
   images: {
-    domains: ['dl.airtable.com'],
+    domains: ['dl.airtable.com', 'v5.airtableusercontent.com'],
   },
   i18n: {
     locales: ['ko', 'en'],
