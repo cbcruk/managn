4cac755f8177889c84dde073017ae3356c712cb1
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-03-17 00:10:26 +0900

    add: seed 파일 추가

--- db/schema.ts
+++ db/schema.ts
@@ -2,19 +2,19 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const authors = sqliteTable('authors', {
   id: integer('id').primaryKey(),
-  name_ko: text('name_ko'),
-  name_ja: text('name_ja'),
+  name_ko: text('name_ko').notNull(),
+  name_ja: text('name_ja').notNull(),
 })
 
 export const books = sqliteTable('books', {
   id: integer('id').primaryKey(),
-  status: text('status'),
+  status: text('status').notNull(),
   link: text('link'),
-  title_ko: text('title_ko'),
-  title_ja: text('title_ja'),
+  title_ko: text('title_ko').notNull(),
+  title_ja: text('title_ja').notNull(),
 })
 
 export const book_authors = sqliteTable('book_authors', {
-  book_id: integer('book_id'),
-  author_id: integer('author_id'),
+  book_id: integer('book_id').notNull(),
+  author_id: integer('author_id').notNull(),
 })
