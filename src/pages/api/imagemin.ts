2a3c1f7417052ff52958ad7c64dfcd9b6ddcf659
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-05-04 21:53:36 +0900

    update: api/imagemin POST 처리 추가

--- src/pages/api/imagemin.ts
+++ src/pages/api/imagemin.ts
@@ -1,11 +1,16 @@
+import type { APIRoute } from 'astro'
 import imagemin from 'imagemin'
 import imageminWebp from 'imagemin-webp'
 
-await imagemin(['src/content/books/assets/*.{jpg,jpeg,png}'], {
-  destination: 'src/content/books/assets',
-  plugins: [
-    imageminWebp({
-      quality: 50,
-    }),
-  ],
-})
+export const POST: APIRoute = async () => {
+  await imagemin(['src/content/books/assets/*.{jpg,jpeg,png}'], {
+    destination: 'src/content/books/assets',
+    plugins: [
+      imageminWebp({
+        quality: 50,
+      }),
+    ],
+  })
+
+  return new Response(JSON.stringify({ message: 'SUCCESS' }), { status: 200 })
+}
