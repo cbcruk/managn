ac9832de000b23b41ed491557c8db0695f451939
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-12-31 07:36:00 +0900

    fix: `leftJoin` 방식으로 query 수정

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,24 +6,39 @@ import {
 } from 'astro:content'
 import { db } from 'db/managn'
 import * as schemas from 'db/schema'
+import { and, eq, getTableColumns, sql } from 'drizzle-orm'
 
 const books = defineCollection({
   async loader() {
-    const bookAuthorRows = await db.select().from(schemas.book_authors)
-    const bookRows = await db.select().from(schemas.books)
+    const bookRows = await db
+      .select({
+        ...getTableColumns(schemas.books),
+        authors: sql<string>`GROUP_CONCAT(authors.id)`,
+      })
+      .from(schemas.books)
+      .leftJoin(
+        schemas.book_authors,
+        eq(schemas.books.id, schemas.book_authors.book_id)
+      )
+      .leftJoin(
+        schemas.authors,
+        eq(schemas.book_authors.author_id, schemas.authors.id)
+      )
+      .where(eq(schemas.books.status, 'release'))
+      .groupBy(schemas.books.id)
 
-    return bookRows.map((book) => {
+    const result = bookRows.map((book) => {
       const cover = new URL(`./books/assets/${book.id}.webp`, import.meta.url)
 
       return {
         ...book,
         id: `${book.id}`,
-        authors: bookAuthorRows
-          .filter(({ book_id }) => book.id === book_id)
-          .map(({ author_id }) => `${author_id}`),
+        authors: book.authors.split(','),
         cover: cover.pathname,
       }
     })
+
+    return result
   },
   schema: ({ image }) =>
     z.object({
@@ -38,18 +53,31 @@ const books = defineCollection({
 
 const authors = defineCollection({
   async loader() {
-    const bookAuthorRows = await db.select().from(schemas.book_authors)
-    const authorRows = await db.select().from(schemas.authors)
-
-    return authorRows.map((author) => {
+    const authorRows = await db
+      .select({
+        ...getTableColumns(schemas.authors),
+        books: sql<string>`GROUP_CONCAT(books.id)`,
+      })
+      .from(schemas.authors)
+      .leftJoin(
+        schemas.book_authors,
+        eq(schemas.book_authors.author_id, schemas.authors.id)
+      )
+      .leftJoin(
+        schemas.books,
+        eq(schemas.book_authors.book_id, schemas.books.id)
+      )
+      .where(eq(schemas.books.status, 'release'))
+      .groupBy(schemas.authors.id)
+    const result = authorRows.map((author) => {
       return {
         ...author,
         id: `${author.id}`,
-        books: bookAuthorRows
-          .filter(({ author_id }) => author.id === author_id)
-          .map(({ book_id }) => `${book_id}`),
+        books: author.books.split(','),
       }
     })
+
+    return result
   },
   schema: z.object({
     name_ko: z.string().nullable(),
