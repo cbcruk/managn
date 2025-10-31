12501a44e4345d42865feb5f5efa41f650c5e8ec
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-11-01 08:05:37 +0900

    upgrade: next-16

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   if (process.env.NODE_ENV === 'production') {
     return NextResponse.redirect(new URL('/', request.url))
   }
